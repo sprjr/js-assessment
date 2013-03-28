dcd36dcf3a3eba578d2f2e3fbd0ebc2103e30d1c
Stephen Rivas JR <stephen@zeunic.com>
2013-03-28 00:40:22 -0400

    Array sum done

--- app/arrays.js
+++ app/arrays.js
@@ -7,7 +7,11 @@ define(function() {
     },
 
     sum : function(arr) {
-
+        var results = 0;
+        for (var i=0, j=arr.length; i<j; i++) {
+            results += arr[i];
+        }
+        return results;
     },
 
     remove : function(arr, item) {
