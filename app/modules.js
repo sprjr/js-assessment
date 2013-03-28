ffad56f9774b512781247e2017e4469da2a64166
Stephen Rivas JR <stephen@zeunic.com>
2013-03-28 00:33:54 -0400

    Module tests complete.

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
+        var Module = function(greeting, name){
+            return {
+                sayIt: function(){
+                    return this.greeting + ', ' + this.name;
+                },
+                name: name,
+                greeting: greeting
+            };
+        };
 
+        return new Module(str1, str2);
     }
   };
 });
