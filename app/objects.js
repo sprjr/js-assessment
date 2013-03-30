5af22e5aba67203d76e3737ba2653fb1a94e6f23
Stephen Rivas JR <stephen@zeunic.com>
2013-03-30 19:12:48 -0400

    Objects and Context Complete

--- app/objects.js
+++ app/objects.js
@@ -1,17 +1,28 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
-  return {
-    alterContext : function(fn, obj) {
+	return {
+		alterContext : function(fn, obj) {
+			return fn.call(obj);
+		},
 
-    },
+		alterObjects : function(constructor, greeting) {
+			constructor.prototype.greeting = greeting;
+			return constructor;
+		},
 
-    alterObjects : function(constructor, greeting) {
+		iterate : function(obj) {
+			// Object.getOwnPropertyNames(obj);
 
-    },
+			var ownProperties = [];
 
-    iterate : function(obj) {
+			for(var key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					ownProperties.push(key + ': ' + obj[key]);
+				}
+			}
 
-    }
-  };
+			return ownProperties;
+		}
+	};
 });
