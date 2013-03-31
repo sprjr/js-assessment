7cab07d5fd0d66580d100344f64e80e9a1a5c037
Stephen Rivas JR <stephen@zeunic.com>
2013-03-31 04:33:04 -0400

    Recursion (is tricky) step 1 down, now to clean up the logic for the sub dir specified portion

--- app/recursion.js
+++ app/recursion.js
@@ -1,13 +1,32 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
-  return {
-    listFiles: function(data, dirName) {
+	return {
+		listFiles: function(data, dirName) {
+			var recurseFileList = [];
 
-    },
+			if(data.files) {
+				for (var i = 0; i < data.files.length; i++) {
+					var item = data.files[i];
+					if(typeof item === 'string') {
+						if(dirName !== undefined && dirName === data.dir) {
+							// console.log('directory sent: ' + dirName, data.dir);
+							recurseFileList.push(item);
+						} else {
+							// console.log('directory not sent: ' + dirName, data.dir);
+							recurseFileList.push(item);
+						}
+					} else if(typeof item === 'object') {
+						recurseFileList = recurseFileList.concat( this.listFiles(item, dirName) );
+					}
+				}
+			}
 
-    permute: function(arr) {
+			return recurseFileList;
+		},
 
-    }
-  };
+		permute: function(arr) {
+
+		}
+	};
 });
