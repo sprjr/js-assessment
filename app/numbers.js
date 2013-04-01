5312032c0c82e28d770390f119b0355706de3386
Stephen Rivas JR <stephen@zeunic.com>
2013-03-31 23:24:52 -0400

    Not doing very well with the whole numbers.js stuff. Need to up my game here.

--- app/numbers.js
+++ app/numbers.js
@@ -1,22 +1,31 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
-  return {
-    valueAtBit: function(num, bit) {
+	return {
+		valueAtBit: function(num, bit) {
 
-    },
+		},
 
-    base10: function(str) {
+		base10: function(str) {
+			return parseInt(str, 2);
+		},
 
-    },
+		convertToBinary: function(num) {
+			// this is the answer, but I do not understand it
+			// as such, I do not intend to activate it to pass the test
+			// and want to sit down and work through it
 
-    convertToBinary: function(num) {
+			// var arr = [];
 
-    },
+// 			for (var i = 7; i > -1; i--) {
+// 				arr.push( num & (1 << i) ? 1 : 0 );
+// 			}
 
-    multiply: function(a, b) {
+// 			return arr.join('');
+		},
 
-    }
-  };
+		multiply: function(a, b) {
+			return a * b;
+		}
+	};
 });
-
