0e30f75e14bdc5f6ad0b84fccdd8eb42fc40c55d
Stephen Rivas JR <stephen@zeunic.com>
2013-04-09 11:07:48 -0400

    Working through some of the basic regex tests

--- app/regex.js
+++ app/regex.js
@@ -3,15 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return (/[0-9]/).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        return (/([A-z])\1+/).test(str);
     },
 
     endsWithVowel : function(str) {
-
+        return (/[aeiou]$/i).test(str);
     },
 
     captureThreeNumbers : function(str) {
