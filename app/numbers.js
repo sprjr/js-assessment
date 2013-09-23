488112d687a8387eb6ba5f0d3fac022daa22bbd3
Stephen Rivas <srivas@thuzi.com>
2013-09-23 09:45:10 -0400

    implementing precision multiplication

--- app/numbers.js
+++ app/numbers.js
@@ -25,7 +25,32 @@ define(function() {
 		},
 
 		multiply: function(a, b) {
-			return a * b;
+			// after researching stack, some articles, and the answer sheet
+			// I found the answer to be the most applicable, and concise solution
+			// Although I believe there are libraries to assist with this (SinfulJS?)
+			// if this is a large part of the application
+
+			a = adjust(a);
+			b = adjust(b);
+
+			return (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+			function adjust (num) {
+				var exponent = null,
+					result = {
+						adjusted: num,
+						multiplier: 1
+					};
+
+				if (num < 1) {
+					exponent = Math.floor(Math.log(num) * -1);
+
+					result.multiplier = Math.pow(10, exponent);
+					result.adjusted = num * result.multiplier;
+				}
+
+				return result;
+			}
 		}
 	};
 });
