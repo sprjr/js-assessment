51d55a1230b2fd61c924fecf6a7c0008d14dedc0
Stephen Rivas <srivas@thuzi.com>
2013-11-26 23:46:00 -0500

    finishing up async tests

--- app/async.js
+++ app/async.js
@@ -3,11 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+        var deferred = $.Deferred();
 
+        setTimeout(function () { deferred.resolve(value); }, 1337);
+
+        return deferred;
     },
 
     manipulateRemoteData : function(url) {
+        var deferred = $.Deferred();
 
+        $.ajax(url)
+            .then(function (data) {
+                var people = _.chain(data.people)
+                    .map(function (person) {
+                        return person.name;
+                    })
+                    .value()
+                    .sort();
+
+                deferred.resolve(people);
+            });
+
+        return deferred;
     }
   };
 });
